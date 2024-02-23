1d1680ad27cca6d16a3ec6cf327b68d829959541
Kathryn Hornburg <93157856+kjhornbu@users.noreply.github.com>
2024-02-23 16:27:40 -0500

    Left Right Indexing Fix
    Figured out the for loop issue with indexing into arrays

--- tri.py
+++ tri.py
@@ -3,7 +3,7 @@ import numpy as np
 import warnings as warnings
 
 ## Inputs Block ##
-size_triangle = int(input("Give An Integer Row Number for the Triangle: "))
+size_triangle = int(input("Give An Integer Row Number for the Triangle: ")) # This already does the error handling for you for the int issue will not just blindly convert data
 dir_triangle = input("Give Direction of the Triangle (^ for up, v for down, < for left, or > for right): ")
 
 ## Warnings Block ##
@@ -13,9 +13,6 @@ if not(dir_triangle in [">", "<", "^", "v"]):
 if size_triangle < 0:
     warnings.warn("The triangle size needs to be positive")
 
-if type(size_triangle) != int:
-      warnings.warn("The triangle size needs to be an integer")
-        
 ## Layout of Triangle Queue ##                
 if (dir_triangle == ">") or (dir_triangle == "<"):
     triangle_queue=np.zeros((size_triangle+(size_triangle-1), size_triangle))
@@ -25,33 +22,29 @@ if (dir_triangle == ">") or (dir_triangle == "<"):
         for n in range(size_triangle):
             #row,column
             if n == 0:
-                triangle_queue[size_triangle-1][size_triangle-1] = 1
+                triangle_queue[size_triangle-1,size_triangle-1] = 1
             else:
                 check = np.nonzero(triangle_queue[:,-n]==1)
                 
                 position_increase = check[0]+1
                 position_decrease = check[0]-1
 
-                new_positions=np.unique(np.hstack((position_increase, position_decrease)))
-                # For some reason needed to assign each row position into the queue uniquely unlike the columns (up/down triangles).
-                for position in new_positions:
-                    triangle_queue[position][(size_triangle-1)-n] = 1
+                # It was needing the loop because you were doing a weird slice indexing... you specify by [r,c] not [r] [c] once you change to [r,c] you can do it this way
+                triangle_queue[np.unique(np.hstack((position_increase, position_decrease))),(size_triangle-1)-n] = 1
                                    
     elif dir_triangle == "<":
     #The Left Facing Triangle Queue Block
         for n in range(size_triangle):
             if n == 0: 
-                triangle_queue[size_triangle-1][n] = 1
+                triangle_queue[size_triangle-1,n] = 1
             else:
                 check = np.nonzero(triangle_queue[:,n-1]==1)
                 
                 position_increase = check[0]+1
                 position_decrease = check[0]-1
                 
-                new_positions=np.unique(np.hstack((position_increase, position_decrease))) 
-                # For some reason needed to assign each row position into the queue uniquely unlike the columns (up/down triangles).
-                for position in new_positions:
-                    triangle_queue[position][n] = 1
+                # It was needing the loop because you were doing a weird slice indexing... you specify by [r,c] not [r] [c] once you change to [r,c] you can do it this way
+                triangle_queue[np.unique(np.hstack((position_increase, position_decrease))),n] = 1
 
 elif (dir_triangle == "^") or (dir_triangle == "v"): 
     triangle_queue=np.zeros((size_triangle,size_triangle+(size_triangle-1)))
@@ -60,27 +53,27 @@ elif (dir_triangle == "^") or (dir_triangle == "v"):
         #The Up Facing Triangle Queue Block
         for n in range(size_triangle):
             if n == 0:
-                triangle_queue[n][size_triangle-1] = 1
+                triangle_queue[n,size_triangle-1] = 1
             else:
                 check = np.nonzero(triangle_queue[n-1,:]==1)
                 
                 position_increase = check[0]+1
                 position_decrease = check[0]-1
                 
-                triangle_queue[n][np.unique(np.hstack((position_increase, position_decrease)))] = 1
+                triangle_queue[n,np.unique(np.hstack((position_increase, position_decrease)))] = 1
                 
     elif dir_triangle == "v":
         #The down Facing Triangle Queue Block
         for n in range(size_triangle):   
             if n == 0:
-                triangle_queue[size_triangle-1][size_triangle-1] = 1
+                triangle_queue[size_triangle-1,size_triangle-1] = 1
             else:
                 check = np.nonzero(triangle_queue[-n,:]==1)
                 
                 position_increase = check[0]+1
                 position_decrease = check[0]-1
                 
-                triangle_queue[-n-1][np.unique(np.hstack((position_increase, position_decrease)))] = 1
+                triangle_queue[-n-1,np.unique(np.hstack((position_increase, position_decrease)))] = 1
         
 ## Setting up Triangle String Array based on Triangle Queue Data ##  
 triangle = np.chararray((triangle_queue.shape[0], triangle_queue.shape[1]))
